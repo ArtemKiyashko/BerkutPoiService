a91869d9842fa6d7879ceb0bbb7b74dd6919af5e
Artem Kiyashko <artem86@hotmail.co.uk>
2023-10-17 17:11:10 +0200

    init

---
+++ BerkutPoiService/PoiService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace BerkutPoiService
+{
+    public class PoiService
+    {
+        [FunctionName("nearest")]
+        public async Task<IActionResult> Get(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] GpsRequest gpsRequest)
+        {
+            return new OkObjectResult();
+        }
+    }
+}
+
